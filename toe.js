d5aeb9fd3f50998334885aaf1f1fef72a3849291
Julius Reade <Julius_dott@hotmail.com>
2016-05-21 23:28:00 +1000

    need to figure out how to maintain state logic with loop. good learning curve

--- toe.js
+++ toe.js
@@ -1,16 +1,32 @@
 // further testing by breaking things down. 
+var combination = { 
+	h1: [0,0,0]
+};
+
+var t1 = document.getElementById('t1');
+var t2 = document.getElementById('t2');
+var t3 = document.getElementById('t3');
+
+var t3Event = t3.addEventListener('click', function() {t3E(p)}, false);
+var t2Event = t2.addEventListener('click', function() {t2E(p)}, false);
+var t1Event = t1.addEventListener('click', function() {t1E(p)}, false);
 
 
-	for (var i = 0; i < length.buffer; i++) {
-	
+var buffer = [true,false,true,false,true,false,true,false,true]
+
+
+
+// maybe seperate three. 
+	for (var i = 0; i < buffer.length; i++) {
+
 	// event function
 		if (buffer[i] === true) {
 			var p = "X";
 		} else {
 			var p = "O";
-		} 
+		};
 
-		function t1E(player) {
+		function t1E(p) {
 			if (p === "X") {
 				t1.innerHTML = "X";
 				combination.h1[0] = 1;
@@ -19,10 +35,29 @@
 				t1.innerHTML = "O";
 				combination.h1[0] = 2;
 			}
-			determineWinner();
+		};
+		function t2E(p) {
+			if (p === "X") {
+				t2.innerHTML = "X";
+				combination.h1[1] = 1;
+			}
+			else {
+				t2.innerHTML = "O";
+				combination.h1[1] = 2;
+			}
 		};
 
-		var t1Event = t1.addEventListener('click', function() {t1E(p)}, false);
+		function t3E(player) {
+			if (p === "X") {
+				t3.innerHTML = "X";
+				combination.h1[2] = 1;
+			}
+			else {
+				t3.innerHTML = "O";
+				combination.h1[2] = 2;
+			}
 
+};
+};
 
-	}
+// so the good thing is 
